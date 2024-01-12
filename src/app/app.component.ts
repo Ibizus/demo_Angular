a541703f93d7dc47c926d5ef16e18d2b531d98f9
Hector Lopez <117910847+Ibizus@users.noreply.github.com>
2024-01-12 14:35:00 +0100

    añado componentes nuevos sin crear aun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavbarComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'mi-primer-proyecto';
+
+  // mostrar:boolean = true;
+  // lista:string[] = ["uno", "dos", "tres"];
+
+  // cambiar(){
+  //   this.mostrar = !this.mostrar;
+  // }
 }
