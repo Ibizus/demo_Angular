1eb82e16cc364568d08bb49d1cf4eda5919f02a3
Hector Lopez <117910847+Ibizus@users.noreply.github.com>
2024-01-15 11:10:55 +0100

    practica de clase hecha

--- src/app/componentes/main/main.component.ts
+++ src/app/componentes/main/main.component.ts
@@ -1,12 +1,20 @@
+import { NgIf, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
+  mostrar:boolean = true;
+
+  mostrarOcultar(){
+    this.mostrar = !this.mostrar;
+  }
+
+  mylist:string[] = ["Carlos", "Miguel", "Juan", "Pedro"];
 }
